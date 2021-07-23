83a0ce663ea5f9cb4c2f570e28c8fae7cd7a8f88
tomoya shibazaki <tshibazaki8165@gmail.com>
2021-07-23 10:06:06 +0900

    add: select box

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import SelectBox from "./SelectBox";
 
 const ProductComponent = () => {
     const products = useSelector((state) => state.allProducts.products)
     const renderList = products.map((product) => {
         const { id, title, price, category } = product
+
         return(
-          <tr>
+          <tr key={id}>
             <td data-label="title">{title}</td>
-            <td data-label="price">{price}</td>
+            <td data-label="price">$ {price}</td>
             <td data-label="category">{category}</td>
+            <td data-label="select"><SelectBox /></td>
+            <td data-label="delete"><button>delete</button></td>
           </tr>
         )
     })
@@ -21,6 +25,8 @@ const ProductComponent = () => {
                 <th data-label="title">title</th>
                 <th data-label="price">price</th>
                 <th data-label="category">category</th> 
+                <th data-label="select">select box</th>
+                <th data-label="delete">delete</th>
               </tr>
             </thead>
           <tbody>
